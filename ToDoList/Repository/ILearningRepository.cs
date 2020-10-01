d983f953c2a8fe96d81e68e59a2536b106ab1e66
grzegorzzukowskigda@gmail.com <grzegorzzukowskigda@gmail.com>
2020-10-01 09:44:48 +0200

    implement repoitory pattern

---
+++ ToDoList/Repository/ILearningRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Model;
+
+namespace ToDoList.Repository
+{
+    public interface ILearningRepository
+    {
+        IEnumerable<LearningThing> GetAllLearningThings();
+        LearningThing GetLearningThingById(int id);
+        LearningThing GetLearningThingByCategoryDescription(string description);
+        void CreateLearningThing(LearningThing learningThing);
+        void UpdateLearningThing(LearningThing learningThing);
+        void DeleteLearningThing(int id);
+    }
+}
---
+++ ToDoList/Repository/LearningThingRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToDoList.Model;
+
+namespace ToDoList.Repository
+{
+    class LearningThingRepository: ILearningRepository
+    {
+        private List<LearningThing> context;
+
+        public LearningThingRepository(List<LearningThing> context)
+        {
+            this.context = context;
+        }
+
+        public void CreateLearningThing(LearningThing learningThing)
+        {
+            context.Add(learningThing);
+        }
+
+        public void DeleteLearningThing(int id)
+        {
+            LearningThing learningThing = context.Find(x => x.Id.Equals(id));
+            context.Remove(learningThing);
+        }
+
+        public IEnumerable<LearningThing> GetAllLearningThings()
+        {
+            return context.ToList();
+        }
+
+        public LearningThing GetLearningThingByCategoryDescription(string description)
+        {
+            return context.Find(x => x.Description.Equals(description));
+        }
+
+        public LearningThing GetLearningThingById(int id)
+        {
+            return context.Find(x => x.Id.Equals(id));
+        }
+
+        public void UpdateLearningThing(LearningThing learningThing)
+        {
+            LearningThing learningThingToUpdate = context.Find(x => x.Id.Equals(learningThing.Id));
+            learningThingToUpdate = learningThing;
+            context.Add(learningThingToUpdate);
+        }
+    }
+}
